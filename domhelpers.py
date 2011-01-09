880b07a430748ae5ef10debd55a053cddcceb5ed
Ben Robinson <bfrobin446@gmail.com>
2011-01-09 12:30:53 -0600

    Added getChildElementsWithTagName() to domhelpers

--- domhelpers.py
+++ domhelpers.py
@@ -7,6 +7,11 @@ def getFirstChildElementWithTagName(parentNode, tagName):
     except StopIteration:
         return None
 
+def getChildElementsWithTagName(parentNode, tagName):
+    return [child for child in parentNode.childNodes
+            if child.nodeType == child.ELEMENT_NODE
+            and child.tagName == tagName]
+
 def getInnerText(node):
     if node.nodeType == node.TEXT_NODE or node.nodeType == node.CDATA_SECTION_NODE:
         return node.data
