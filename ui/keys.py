2563e9fdbdf1bb1658c7afb51d227230b56521db
Ben Robinson <bfrobin446@gmail.com>
2011-05-04 17:01:46 -0500

    Added prefs interface for key bindings. Added key category and a few keys for tactical map.

--- ui/keys.py
+++ ui/keys.py
@@ -3,20 +3,37 @@ from PyQt4.QtGui  import *
 
 from collections import OrderedDict
 
+from notifier import Notifier
+
+changeNotifier = Notifier()
+
 class Key(object):
     settings = QSettings()
     settings.beginGroup('keys')
+    
     def __init__(self, name, default, displayname):
         self.default = default
         self.name = name
         self.displayname = displayname
         self.current = self.settings.value(self.name, self.default)
 
+    def update(self, newKey):
+        self.settings.setValue(self.name, newKey)
+        self.current = newKey
+        changeNotifier.trigger()
+
+
 def defineKey(category, name, default, displayname):
     globals()[category][name] = Key('/'.join((category, name)), default, displayname)
 
-categories = ("galaxy", )
+categories = ("galaxy", "tactical")
+catnames   = ("Galaxy map", "Tactical view")
 for cat in categories:
     globals()[cat] = OrderedDict()
 
 defineKey("galaxy", "wait", Qt.Key_Space, "Cancel destination / Wait")
+defineKey("galaxy", "dash", Qt.Key_Shift, "Use dash speed")
+
+defineKey("tactical", "thrust", Qt.Key_Up, "Forward thrust")
+defineKey("tactical", "steerLeft", Qt.Key_Left, "Turn left")
+defineKey("tactical", "steerRight", Qt.Key_Right, "Turn right")
--- ui/prefs.py
+++ ui/prefs.py
@@ -3,8 +3,42 @@ from PyQt4.QtGui  import *
 
 import itertools
 
+from .            import colors
+from .            import keys
 from .colorbutton import ColorButton
-from . import colors
+
+
+class KeyPicker(QLineEdit):
+    keyChanged = pyqtSignal(Qt.Key)
+
+    def __init__(self, key=None, parent=None, flags=Qt.Widget, **kwargs):
+        QLineEdit.__init__(self, parent, **kwargs)
+        self.setWindowFlags(flags)
+        self.setReadOnly(True)
+        self.key = key
+        self.setText(self.textForKey(self.key))
+
+    @staticmethod
+    def textForKey(k):
+        for name, value in Qt.__dict__.items():
+            if name[0:4] == 'Key_':
+                if k == value:
+                    return name
+        return ''
+
+    def focusInEvent(self, evt):
+        self.setText("<press a key>")
+
+    def keyPressEvent(self, evt):
+        self.key = evt.key()
+        self.setText(self.textForKey(self.key))
+        self.keyChanged.emit(self.key)
+
+    def setKey(self, key):
+        self.key = key
+        self.setText(self.textForKey(self.key))
+        self.keyChanged.emit(self.key)
+
 
 class PrefsDialog(QDialog):
     def __init__(self, parent=None, flags=Qt.Widget):
@@ -21,28 +55,35 @@ class PrefsDialog(QDialog):
                 )
         self.layout().addWidget(self.buttons)
 
-        self.buttons.button(self.buttons.RestoreDefaults).clicked.connect(self.defaults)
+        self.buttons.button(
+                self.buttons.RestoreDefaults).clicked.connect(self.defaults)
 
         self.addTab(ColorPrefPane(), "Colors")
+        self.addTab(KeyPrefPane(), "Keys")
 
     def addTab(self, widget, title):
         scroller = QScrollArea()
         scroller.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         scroller.setWidget(widget)
-        scroller.setMinimumWidth(widget.sizeHint().width() +
-                qApp.style().pixelMetric(QStyle.PM_ScrollBarExtent, None, scroller) * 2)
+        scroller.setMinimumWidth(
+                widget.sizeHint().width()
+                + qApp.style().pixelMetric(
+                    QStyle.PM_ScrollBarExtent, None, scroller)
+                * 2)
         widget.resize(widget.sizeHint())
         self.tabs.addTab(scroller, title)
 
     def defaults(self):
         self.tabs.currentWidget().widget().defaults()
 
+
 class ColorPrefPane(QWidget):
     def __init__(self, parent=None, flags=Qt.Widget):
         QWidget.__init__(self, parent, flags)
         self.setLayout(QVBoxLayout(self))
         for cat, catname in zip(colors.categories, colors.catnames):
-            self.layout().addWidget(QLabel(''.join(('<b><big>', catname, '</big></b>')), self))
+            self.layout().addWidget(
+                    QLabel(''.join(('<b><big>', catname, '</big></b>')), self))
             catLayout = QGridLayout()
             self.layout().addLayout(catLayout)
             for i, c in enumerate(getattr(colors, cat).values()):
@@ -57,3 +98,26 @@ class ColorPrefPane(QWidget):
                 (obj for obj in self.children() if isinstance(obj, ColorButton))
                 ):
             picker.setColor(color.default)
+
+
+class KeyPrefPane(QWidget):
+    def __init__(self, parent=None, flags=Qt.Widget):
+        QWidget.__init__(self, parent, flags)
+        self.setLayout(QVBoxLayout(self))
+        for cat, catname in zip(keys.categories, keys.catnames):
+            self.layout().addWidget(
+                    QLabel(''.join(('<b><big>', catname, '</big></b>')), self))
+            catLayout = QGridLayout()
+            self.layout().addLayout(catLayout)
+            for i, c in enumerate(getattr(keys, cat).values()):
+                catLayout.addWidget(QLabel(c.displayname, self), i, 1)
+                picker = KeyPicker(c.current, self, keyChanged = c.update)
+                catLayout.addWidget(picker, i, 0)
+
+    def defaults(self):
+        for key, picker in zip(
+                itertools.chain.from_iterable(
+                    getattr(keys, cat).values() for cat in keys.categories),
+                (obj for obj in self.children() if isinstance(obj, KeyPicker))
+                ):
+            picker.setKey(key.default)
